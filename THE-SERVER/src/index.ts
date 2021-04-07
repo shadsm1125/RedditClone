01cdff6c6de6b40fd197946df215051c09dce6a7
shadsm1125 <shadburn.sean@gmail.com>
2021-04-07 08:50:23 -0400

    setting up of mikro-orm

--- THE-SERVER/src/index.ts
+++ THE-SERVER/src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello wld!")
+import {MikroORM} from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+
+    await orm.getMigrator().up(); 
+
+    const post = orm.em.create(Post, {title: "first post"});
+
+    await orm.em.persistAndFlush(post);
+}
+
+main();
